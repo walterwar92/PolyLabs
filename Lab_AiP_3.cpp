1e36999fc886b8307e3e604bf24fe3bfb8567d83
walterwar92 <46653416+walterwar92@users.noreply.github.com>
2023-11-23 11:35:45 +0300

    Update Lab_AiP_3.cpp
    not std::

--- Lab_AiP_3.cpp
+++ Lab_AiP_3.cpp
@@ -3,29 +3,32 @@
 #include <string>
 #include <locale>
 
-std::string reverseString(const std::string& str) {
-    return std::string(str.rbegin(), str.rend());
+using namespace std;
+
+// Функция, которая переворачивает строку
+string reverseString(const string& str) {
+    return string(str.rbegin(), str.rend());
 }
 
 int main() {
-    std::ifstream inputFile("input.txt"); // Имя файла для чтения
+    ifstream inputFile("input.txt"); // Имя файла для чтения
 
     if (!inputFile.is_open()) {
-        std::cout << "Unable to open file!" << std::endl;
+        cout << "Unable to open file!" << endl;
         return 1;
     }
 
-    std::string word;
-    std::locale loc("ru_RU.UTF-8"); // Установка локали для корректной обработки русских букв
+    string word;
+    locale loc("ru_RU.UTF-8"); // Установка локали для корректной обработки русских букв
 
     while (inputFile >> word) {
         // Если текущее слово содержит буквы, обрабатываем их
         for (char& c : word) {
-            if (std::isalpha(c, loc)) {
-                c = std::tolower(c, loc); // Приводим все буквы к нижнему регистру для корректного переворота
+            if (isalpha(c, loc)) {
+                c = tolower(c, loc); // Приводим все буквы к нижнему регистру для корректного переворота
             }
         }
-        std::cout << reverseString(word) << " ";
+        cout << reverseString(word) << " ";
     }
 
     inputFile.close();
